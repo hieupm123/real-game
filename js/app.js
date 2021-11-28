9babb87b50d258b82a670c64bbce2c889476a402
your_github_username <hello@hlleo>
2021-11-28 15:29:11 +0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,41 +1,112 @@
-var phrases = ['','','Goc nhap xa tang tu Bac vao Nam','','','su phan hoa cua thanh 3 dai dia hinh','',
-'Truong Son Đong','','Gio mua va huong cac day nui','','','Mat me','','Phan lon dia hinh la doi nui','Khi hau'
-,'','Bac và Đong Bac Bac Bo','Tinh chat nhiet đoi tang dan','',''];
-var phrases_by_num = [3,2,0,3,3,0,2,0,2,0,4,3,0,1,0,0,4,0,0,3,2];
-var qs = ['Nguyên nhân chính làm thiên nhiên phân hóa theo Bắc – Nam là sự phân hóa của'
-,'Cảnh quan thiên nhiên tiêu biểu của phần lãnh thổ phía Bắc (từ dãy Bạch Mã trở ra)'
+var phrases = [''
+,'Khi hau'
+,''
+,''
+,''
+,''
+,''
+,'Truong Son Dong'
+,''
+,''
+,''
+,''
+,'Mat me'
+,''
+,''
+,'Khi hau'
+,''
+,''
+,''
+,''
+,''
+,''];
+// kết quả cần điền với bài text chú ý bỏ hết đấu
+// còn không 1 ngoặc kép
+var phrases_by_num = [2,0,1,3,3,2,2,0,2,4,4,3,0,1,2,0,4,3,4,3,1,2];
+// nếu là điền ô chữ thì là 0
+// đáp án nếu A thì là 1, B là 2, ...
+var qs = ['Cảnh quan thiên nhiên tiêu biểu của phần lãnh thổ phía Bắc ( từ dãy Bạch Mã trở ra)'
+,'Nguyên nhân chính làm thiên nhiên phân hóa theo Bắc – Nam là sự phân hóa của'
 ,'Nguyên nhân chính khiến nhiệt độ tăng từ Bắc và Nam là'
-,'Đặc điểm nào sau đây không thuộc khí hậu phần lãnh thổ phía Nam?'
+,'Đặc điểm nào sau đây KHÔNG thuộc khí hậu phần lãnh thổ phía Nam?'
+,'Thiên nhiên phần lãnh thổ phía Bắc ( từ dãy Bạch Mã trở ra) đặc trưng cho vùng khí hậu nào?'
 ,'Thiên nhiên phần lãnh thổ phía Bắc ( từ dãy Bạch Mã trở ra) đặc trưng cho vùng khí hậu nào'
-,'Sự phân hóa thiên nhiên theo chiều Đông – Tây thể hiện rõ nhất ở'
 ,'Từ đông sang tây, từ biển vào đất liền, thiên nhiên nước ta có sự phân hóa thành 3 dải rõ rệt là'
-,'Vùng đồi núi có mùa mưa và mùa thu và chịu tác động của gió tây khô nóng vào mùa hạ là đặc điểm của','Đặc điểm của thềm lục địa miền Trung là','Nguyên nhân chính làm thiên nhiên phân hóa theo Đông – Tây ở vùng đồi núi phức tạp là do','Đai ôn đới gió mùa trên núi( độ cao từ 2600m trở lên) có đặc điểm khí hậu'
-,'Đai nhiệt đới gió mùa chiếm chủ yếu trong 3 đai cao vì'
+,'Vùng đồi núi có mùa mưa và mùa thu và chịu tác động của gió tây khô nóng vào mùa hạ là đặc điểm của:'
+,'Đặc điểm của thềm lục địa miền Trung là:'
+,'Nguyên nhân chính làm thiên nhiên phân hóa theo Đông – Tây ở vùng đồi núi phức tạp là do:'
+,'Đai ôn đới gió mùa trên núi( độ cao từ 2600m trở lên) có đặc điểm khí hậu'
+,'Đai nhiệt đới gió mùa chiếm chủ yếu trong 3 đai cao vì:'
 ,'Khí hậu ở đai cận nhiệt đới gió màu trên núi có đặc điểm là'
-,'Hệ sinh thái đặc trưng của đai cận nhiệt đới gió mùa trên núi là'
-,'Nguyên nhân dẫn đến thiên nhiên phân hóa theo đai áp cao ở nước ta là'
-,'Thiên nhiên nước ta có 3 đai cao là do sự thay đổi theo độ cao của'
+,'Hệ sinh thái đặc trưng của đai cận nhiệt đới gió mùa trên núi là:'
+,'Nguyên nhân dẫn đến thiên nhiên phân hóa theo đai cao ở nước ta là:'
+,'Thiên nhiên nước ta có 3 đai cao là do sự thay đổi theo độ cao của:'
 ,'Miền Nam Trung Bộ và Nam Bộ có đặc điểm địa hình nào dưới đây?'
 ,'Sự bất thường của nhịp điệu mùa khí hậu, của dòng chảy sông ngòi và tính bất ổn định cao của thời tiết là những trở ngại lớn trong việc sử dụng thiên nhiên của vùng:'
-,'So với miền Bắc và Đông Bắc Bắc bộ, khí hậu miền Tây Bắc và Bắc Trung Bộ có đặc điểm'
+,'So với miền Bắc và Đông Bắc Bắc bộ, khí hậu miền Tây Bắc và Bắc Trung Bộ có đặc điểm:'
 ,'Nguyên nhân khiến cho khí hậu của miền Bắc và và Đông Bắc Bắc Bộ lạnh hơn miền Tây Bắc và Bắc Trung Bộ là do'
-,'Những trở ngại lớn trong quá trình sử dụng tự nhiên của miền Nam Trung Bộ và Nam Bộ là'
-];
-var dao = ['','','Góc nhập xạ &&&& từ &&& vào &&&','','','sự phân hóa của thành 3 dải &&& &&&&','','&&&&&& Sơn &&&&','','&&& mùa và hướng các &&& núi',''
-,'','&&& &&','','Phần lớn &&& &&&& && &&& &&&','&&& &&&','','&&& và Đông &&& &&& Bộ'
-,'Tính chất &&&&& &&& tăng dần','',''];
-var type = [1,1,0,1,1,0,1,0,1,0,1,1,0,1,0,0,1,0,0,1,1];
-var tick = [['Địa hình','Vị trí địa lí','Khí hậu','Hướng núi'],['Đới rừng cận xích đạo gió mùa','Đới rừng nhiệt đới gió mùa','Đới rừng xích đạo ','Đới rừng lá kim'],[]
-,['Nhiệt độ trung bình năm trên 25 độ','Biên độ nhiệt trung bình năm nhỏ','Biên độ nhiệt trung bình năm lớn','Biên độ nhiệt trung bình năm lớn'],
-['Cận nhiệt đơi hải dương','Nhiệt đới lục địa','Nhiệt đới ẩm gió mùa có mùa đông lạnh','Cận xích đạo gió mùa'],[],['Vùng biển- vùng đất – vùng trời','Vùng biển và thềm lục địa – vùng đồng bằng ven biển – vùng đồi núi','Vùng biển và thềm lục địa – vùng đồi núi thấp – vùng đồi núi cao','Vùng biển – vùng đồng bằng – vùng cao nguyên']
-,[],['Mở rộng, đáy nông, có nhiều đảo, nhiều sông lớn đổ ra','Thu hẹp, đáy sâu, có sông, suối nhỏ đổ ra biển','Diện tích rộng, bằng phẳng, có các bãi triều thấp, phẳng','Hẹp ngang, bị chia cắt và phân dải'],[]
-,['Mát mẻ, nhiệt độ trung bình dưới 20 độ','Quanh năm lạnh, nhiệt độ trung bình dưới 10 độ','Mùa hạ nóng (trung bình trên 25 độ), mùa đông lạnh dưới 10 độ','Quanh năm nhiệt độ dưới 15 độ, mùa đông dưới 5 độ']
-,['địa hình núi cao chỉ chiếm 1% diện tích cả nước','địa hình ¾ là đồi núi','đồng bằng và đồi núi thấp chiểm 85%','đồng bằng chỉ chiếm ¼ diện tích'],[]
-,['Rừng lá kim trên đất feralit có mùn','Rừng gió mùa lá rộng thường xanh','Rừng lá kim trên đất feralit','Rừng cận nhiệt đới lá rộng thường xanh'],[],[]
-,['Các dãy núi có hướng vòng cung mở ra phái bắc','Các dãy núi xem kẽ các thung lung sông cùng hướng tây bắc – đông nam','Nơi duy nhất ở Việt Nam có đủ 3 đai cao','Gồm các khối núi cổ, các sơn nguyên bóc mòn, các cao nguyên badan'],[],[]
-,['ảnh hưởng giảm sút hơn của gió mùa Đông Bắc','Nhờ bức chắn địa hình của dãy Hoàng Liên Sơn cao đồ sộ','Tác động của các cánh cung hút gió mùa đông Bắc','Địa hình chủ yếu là núi cao, cao nguyên']
+,'Trở ngại lớn nhất về khí hậu trong việc sử dụng tự nhiên của miền Bắc và Đông Bắc Bắc Bộ là'
+,'Những trở ngại lớn trong quá trình sử dụng tự nhiên của miền Nam Trung Bộ và Nam Bộ là:'];
+// câu hỏi
+var dao = [''
+,'*** ***'
+,''
+,''
+,''
+,''
+,''
+,'***** *** ****'
+,''
+,''
+,''
+,''
+,'*** ***'
+,''
+,''
+,'*** ***'
+,''
+,''
+,''
+,''
+,''
+,''];
+// nếu là câu điền text thì thì thay toàn bộ đáp án = & còn không 1 ngoặc kép
+var type = [1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1];
+// nếu là câu tn thì để số 1, text số 0
+var tick = [['Đới rừng cận xích đạo gió mùa','Đới rừng nhiệt đới gió mùa','Đới rừng xích đạo','Đới rừng lá kim']
+,[]
+,['Góc nhập xạ tăng  từ Bắc vào Nam','Ảnh hưởng của gió mùa Đông Bắc làm giảm nhiệt độ miền Nam','Do nhiệt độ thiên về khí hậu nhiệt đới','Sự gia tăng ảnh hưởng khối khí lạnh về phí Nam']
+,['Nhiệt độ trung bình năm trên 250C, không có tháng nào dưới 200C','Biên độ nhiệt trung bình năm nhỏ','Biên độ nhiệt trung bình năm lớn','Phân chia thành hai mùa mưa và khô']
+,['Cận nhiệt đơi hải dương ','Nhiệt đới lục địa','Nhiệt đới lục địa','Cận xích đạo gió mùa']
+,['Sự phân hóa thiên nhiên giữa Đông Trường Sơn với Tây Trường Sơn','Sự phân hóa của thành 3 dải địa hình','Sự đối lập về mùa mưa và mùa khô giữa Tây Nguyên và ven biển Trung Bộ','Sự đối lập về mùa mưa và mùa khô giữa Tây Nguyên và Đông Trường Sơn']
+,['Vùng biển- vùng đất – vùng trời','Vùng biển và thềm lục địa – vùng đồng bằng ven biển – vùng đồi núi','Vùng biển và thềm lục địa – vùng đồi núi thấp – vùng đồi núi cao','Vùng biển – vùng đồng bằng – vùng cao nguyên']
+,[]
+,['Mở rộng, đáy nông, có nhiều đảo, nhiều sông lớn đổ ra','Thu hẹp, đáy sâu, có sông, suối nhỏ đổ ra biển','Diện tích rộng, bằng phẳng, có các bãi triều thấp, phẳng','Hẹp ngang, bị chia cắt và phân dải']
+,['Gió mùa và độ cao địa hình','Gió mùa và biển Đông','Hướng các dãy núi và độ cao địa hình','Gió mùa và hướng các dãy núi']
+,['Mát mẻ, nhiệt độ trung bình dưới 20oC','Quanh năm lạnh, nhiệt độ trung bình dưới 10oC','Mùa hạ nóng (trung bình trên 25oC), mùa đông lạnh dưới 10oC','Quanh năm nhiệt độ dưới 15oC, mùa đông dưới 5oC']
+,['địa hình núi cao chỉ chiếm 1% diện tích cả nước','địa hình ¾ là đồi núi','đồng bằng và đồi núi thấp chiểm 85%','đồng bằng chỉ chiếm ¼ diện tích']
+,[]
+,['Rừng lá kim trên đất feralit có mùn','Rừng gió mùa lá rộng thường xanh','Rừng lá kim trên đất feralit','Rừng cận nhiệt đới lá rộng thường xanh']
+,['Đất đai thây đổi ở từng đai','Phần lớn địa hình là đồi núi','Khí hậu thay đổi ở các đai','Do tá động của gió mùa Đông Bắc và gió mùa Tây Nam']
+,['']
+,['Các dãy núi có hướng vòng cung mở ra phái bắc','Các dãy núi xem kẽ các thung lung sông cùng hướng tây bắc – đông nam','Các dãy núi xem kẽ các thung lung sông cùng hướng tây bắc – đông nam','Gồm các khối núi cổ, các sơn nguyên bóc mòn, các cao nguyên badan']
+,['Tây Bắc','Nam Bộ và Nam Trung Bộ','Bắc và Đông Bắc Bắc Bộ','Bắc Trung Bộ']
+,['mùa đông lạnh nhất nước','mùa đông lạnh nhất nước','mùa đông lạnh','tính chất nhiệt đới tăng dần']
+,['Ảnh hưởng giảm sút hơn của gió mùa Đông Bắc','Nhờ bức chắn địa hình của dãy Hoàng Liên Sơn cao đồ sộ','Tác động của các cánh cung hút gió mùa đông Bắc','Địa hình chủ yếu là núi cao, cao nguyên']
+,['Nhịp điệu mùa khí hậu thất thường, thời tiết không ổn định','Chịu tác động trực tiếp của gió mùa đông bắc, mùa đông lạnh','Trong năm có hai mùa rõ rệt, thời tiết không ổn định','Nhiều hiện tượng thời tiết cực đoan, thiên tai như bão, vòi rồng']
 ,['độ dốc sông ngòi lớn','thiếu nước vào mùa khô, ngập lụt trên diện rộng','sự thất thường của nhịp điệu mùa','bão lũ, trượt lở đất, hạn hán']];
+// nếu là tn thì để 4 đáp án không thì để 1 ngoặc kép
+// hết 4 đáp án thì xuống dòng với câu trắc nghiệm
+// nếu là text thì để ngoặc như ví dụ
 
+// chú ý ghi xong phần cần điền thì xuống dòng
+// nếu ghi thêm 1 cái j đấy thì thêm dáu phẩy như mẫu
+// chú ý cứ thế điền vào file này không cần xóa j cả trên là vd câu 1 câu 2
+
+var myMusic= document.getElementById("music_ado");
+
+// function play() {
+// }
 $("div button").mouseenter(function(){
  	$("<audio></audio>").attr({ 
  		'src':'audio/blop.aac', 
@@ -46,6 +117,7 @@ $("div button").mouseenter(function(){
 var prenum = 0;
 var map1 = new Map();
 var cnt = 0;
+var ssp = 0;
 const getRandomPhraseAsArray = arr =>{
     var randomNum = Math.floor(Math.random() * phrases.length);
     var randomPhrases = phrases[randomNum].split('');
@@ -133,6 +205,10 @@ startGameBtn.addEventListener('click', ()=>{
             $('#scoreboard').fadeIn(500); //scoreboard
          }, 500);
     }
+    if(!ssp){
+        myMusic.play();
+    }
+    ssp = 1;
     $('.header').text(qs[prenum]);
     setTimeout(function(){
         $('.header').fadeIn(500); //header
@@ -270,6 +346,7 @@ ct_button.addEventListener('click', () =>{
 const reset = () => {
         if(cnt >= 19){
             alert("Điểm số của bạn:" + kq.toString() + "/20");
+            ssp = 0;
             window.location.href = "./new.html";
         }else{
             prenum++;
